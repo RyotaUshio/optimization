0ea1ed88e21e9d632b5833461dcaed7f03430d8f
牛尾凌太 <ryotaushio@ushioryoutanoMacBook-Pro.local>
2020-11-28 11:36:46 +0900

    初期点の影響を調べるテストスクリプト

--- tmp/test.py
+++ tmp/test.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-x0min, x0max = -2.0, 2.0
-x0num = 5
-y0min, y0max = -2.0, 2.0
-y0num = 5
-kmax = 20000
+x0min, x0max = -10.0, 10.0
+x0num = 10
+y0min, y0max = -10.0, 10.0
+y0num = 10
+kmax = 100000
 base = 10
 exp = -10
 
@@ -19,4 +19,4 @@ x0, y0 = np.meshgrid(x0_, y0_)
 !make $target
 for x0, y0 in zip(x0.flat, y0.flat):
     print(f"x0 = [{x0}, {y0}]")
-    !./$target -k $kmax -b $base -e $exp $x0 $y0
+    !./$target -k $kmax -b $base -e $exp -- $x0 $y0
